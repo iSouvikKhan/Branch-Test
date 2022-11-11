50635832a62a65b23a22206cb6b790691348873a
Souvik <souvik97381@gmail.com>
2022-11-12 00:45:31 +0530

    variables added

--- Launch.java
+++ Launch.java
@@ -1,5 +1,7 @@
 public class Launch{
     public static void main(String[] args){
         System.out.println("Hello World !!");
+        int a = 2;
+        System.out.print(a+" This is variable a");
     }
 }
